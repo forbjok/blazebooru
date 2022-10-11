00a274d941571e9e0a815589d9b437b172f5f4d9
Forb.Jok <forbjok@gmail.com>
2022-10-12 00:22:06 +0200

    fix(frontend): Position restoration delay on back navigation

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -10,11 +10,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(savedPosition);
-        }, 1000);
-      });
+      return savedPosition;
     }
   },
   routes: [
