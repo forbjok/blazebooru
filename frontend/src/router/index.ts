07b6d316e0775bfbfbda8b7d0ffe78e6e7c35be1
Forb.Jok <forbjok@proton.me>
2024-07-25 18:13:57 +0200

    fix: Scroll position not restored when navigating back to browse view

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -12,7 +12,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
-      return savedPosition;
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(savedPosition);
+        }, 100);
+      });
     }
   },
   routes: [
